64b4db68ed517659f1161000ddcff959e65a379d
ktp <nkttkn666@gmail.com>
2022-11-29 14:15:16 +0000

    first commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = Task.all
+    end
   end
 
   def show
@@ -12,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に作成されました'
@@ -44,7 +49,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   # Strong Parameter
