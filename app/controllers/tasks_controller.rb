9750c70ecd79fbfd41cea4d24ad037027887dc2b
ktp <nkttkn666@gmail.com>
2022-12-01 11:18:35 +0000

    third commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit ,:update,:destroy]
   
   def index
     @tasks = current_user.tasks.all
@@ -49,8 +50,16 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
-  # Strong Parameter
+  private
+  
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
